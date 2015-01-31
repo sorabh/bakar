a0122691233f5ce325da081b18e0bbffeb6f37df
Sorabh Gupta <sorabh@eliibleapi.com>
2015-01-31 19:41:55 +0530

    move to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bakar::Application.config.secret_token = 'b32458dadd8df4b988951b6f771fb5de4a003a92af2417d7488c5e483e362e24b4dc2bfefba4ac624048f6b5823e5dc00ae0c5f495ecd40890911bd41b432ab7'
